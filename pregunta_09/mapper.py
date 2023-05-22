5c12212f493628d3128ea83702cd21ff0f54f0ca
jdbuilesm <114514412+jdbuilesm@users.noreply.github.com>
2023-05-22 14:25:02 -0500

    Update mapper.py

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+def clr_spaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    x = x.replace("\t", "")
+    return x
+
+def purpose(x):
+    return   clr_spaces(x[2]) + ";"+  clr_spaces(x[0])+ ";" + clr_spaces(x[1])  
+    #return   int(clear_spaces(x[2]))
+
+
+for line in sys.stdin:
+    line = line.replace("'","")
+    result = line.split('   ')
+    print(purpose(result))
