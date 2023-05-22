c062a627ca88bd58fab68ea017543f9966595a1b
jdbuilesm <114514412+jdbuilesm@users.noreply.github.com>
2023-05-22 14:07:55 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+def clr_spaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    return x
+
+def amount(x):
+    return clr_spaces(x[1].split("-")[1])
+
+for line in sys.stdin:
+    line = line.replace("'","")
+    result = line.split('   ')
+    print(amount(result))
