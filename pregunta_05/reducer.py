4ec55a4fb81871f7d5fb2542cc76c8fa925dbdf6
jdbuilesm <114514412+jdbuilesm@users.noreply.github.com>
2023-05-22 14:08:46 -0500

    Update reducer.py

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+biggest = {}
+
+def set_bigger_purpose(dict_purposes, actual_element):
+    actual_element = actual_element.replace("\n", "")
+    dict_purposes[actual_element] = int(dict_purposes.get(actual_element) or 0) + 1 
+    return dict_purposes
+
+for line in sys.stdin:
+    set_bigger_purpose(biggest, line)
+
+for purpose, amount in biggest.items():
+    print(purpose + "	" + str(amount))
