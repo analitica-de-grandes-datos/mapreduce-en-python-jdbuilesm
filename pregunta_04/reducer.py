eaa2d624ae4efea3104bed70a317efa435db787a
jdbuilesm <114514412+jdbuilesm@users.noreply.github.com>
2023-05-22 14:05:49 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+big = {}
+
+def set_big(dict, actual):
+    element_array = actual.split("*")
+    dict[element_array[0]] = int(dict.get(actual[0]) or 0) + 1 
+    return dict
+
+for line in sys.stdin:
+    set_big(big, line)
+
+for purpose, amount in big.items():
+    print(purpose + "," + str(amount))
