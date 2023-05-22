4ae345c40455249cb0c58ae8e320ea3f35201305
jdbuilesm <114514412+jdbuilesm@users.noreply.github.com>
2023-05-22 14:26:55 -0500

    Update mapper.py

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -11,8 +11,6 @@ def clr_spaces(x):
 
 def purpose(x):
     return   clr_spaces(x[2]) + ";"+  clr_spaces(x[0])+ ";" + clr_spaces(x[1])  
-    #return   int(clear_spaces(x[2]))
-
 
 for line in sys.stdin:
     line = line.replace("'","")
